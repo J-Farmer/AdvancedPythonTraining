82886144c4fc812f98f8ea8a5995364f5ed223a4
Jake Farmer <jfarmer0495@gmail.com>
2020-07-13 11:00:25 -0500

    Create basics.py

---
+++ Basics/basics.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 10:53:51 2020
+
+@author: jfarm
+"""
+
+
+def funct():
+    """This is a docstring!"""
+    
+    print("Hello World!")
+    
+funct()
